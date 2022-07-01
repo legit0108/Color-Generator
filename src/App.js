59f4b0132608def156cc73b6b556e5373948b2fd
legit_123 <legitgamer2345@gmail.com>
2022-07-01 22:50:49 +0530

    Color Generator

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
+
+import Values from 'values.js'
+
+function App() {
+  const[color,setColor] = useState('');
+  const[error,setError] = useState(false);
+  const[list,setList] = useState(new Values('#f15025').all(10));
+  
+  const handleSubmit = (event)=>{
+    event.preventDefault();
+    try{
+      let colors = new Values(color).all(10) // tints , shades 100% divided by 10 (both changing by 10% look at % in UI)
+      setList(colors)
+    }catch(error){
+      // catch run for invalid color id 
+      setError(true);
+    }
+  }
+  return(
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input type = "text" value = {color} onChange = {(event)=>setColor(event.target.value)}placeholder="#f15025"
+          className={`${error?'error':null}`}/>
+          <button className="btn" type="submit">submit</button>
+        </form>
+      </section>
+      <section className="colors">
+       {list.map((color,index)=>{
+         return <SingleColor key = {index} {...color} index = {index}></SingleColor>
+       })}
+      </section>
+    </>
+  );
+}
+
+export default App
